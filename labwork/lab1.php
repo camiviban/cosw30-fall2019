947b851bc37cb4a5ade006f9a460c71c606c5577
EC2 Default User <ec2-user@ip-172-31-86-118.ec2.internal>
2019-08-29 04:10:29 +0000

    Resubmitting full html/php to update

--- labwork/lab1.php
+++ labwork/lab1.php
@@ -1,21 +1,47 @@
-<?php
+<!DOCTYPE html>
+
 <html>
     <head>
+        <title>Labwork 1: Hello World</title>
+        <link href="https://fonts.googleapis.com/css?family=Manjari&display=swap" rel="stylesheet">
+        <style>
+            h1 {background-color: red;
+                color: black;
+                font-family: 'Manjari', sans-serif;
+                font-weight: 700;
+                }
+                
+            body {
+                background-color: black;
+                color: white;
+                font-family: 'Manjari', sans-serif;
+                margin: 10%;
+                }
+                
+            main {
+                border: 10px solid white;
+                padding: 5%;
+                }
+        </style>
     </head>
 
     <body>
-        <h1>Labwork 1: Hello World</h1>
+        <header>
+            <h1>Ona Fong-Surdenas</h1>
+        </header>
+        
+        <main>
+            <p>Hi, my name is Ona and this is my second COSW class! The first one I took was in the spring (COSW 10), which focused on HTML and CSS. ADD MORE STUFF HERE</p>
+            <p><strong>Github address</strong>: <a href target="https://github.com/camiviban" target="_blank">camiviban</a></p>
+        
+            <?php 
+            echo 'This will be the PHP paragraph - change the text!!!. Add two links';
+            ?>
+        
+        </main>
     
         <footer>
-            <p>COSW 30 Fall 2019 - Lab 1</p>
+            <p>COSW 30 Fall 2019 - Lab 1 - Ona</p>
         </footer>
     </body>
 </html>
-
-
-
-
-
-
-
-?>
