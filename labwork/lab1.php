d3846f3db91b9ac24ad1c4dd384a86fbbf8bb8fb
EC2 Default User <ec2-user@ip-172-31-86-118.ec2.internal>
2019-08-29 03:01:37 +0000

    Created labwork 1 ver 001 and added in rough text

---
+++ labwork/lab1.php
@@ -0,0 +1,21 @@
+<?php
+<html>
+    <head>
+    </head>
+
+    <body>
+        <h1>Labwork 1: Hello World</h1>
+    
+        <footer>
+            <p>COSW 30 Fall 2019 - Lab 1</p>
+        </footer>
+    </body>
+</html>
+
+
+
+
+
+
+
+?>
