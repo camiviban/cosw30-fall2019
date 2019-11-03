3ca8fb1b599da409de892fd71e70a8451c4cc109
OnaFS <camiviban@gmail.com>
2019-11-03 23:18:25 +0000

    Added validation to check

--- homework/homework10/crud.php
+++ homework/homework10/crud.php
@@ -13,6 +13,52 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') { // This part is grabbing the data the
     $password= $_POST['password'];
     // Must add validation for input coming in, for assignment (doesn't have it yet, the above is just grabbing data input by user into form)
 
+        // This is for validation and outputting error messages if fields aren't filled out
+          if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+                if (!empty($_REQUEST['firstname'])) { //This checks if FIRST NAME is filled and stickies it. If not, it gives error message
+                 $firstname = $_REQUEST['firstname'];
+                    } else {
+                        $firstname = NULL;
+                        echo "<p class=\"error_message\">Please enter your first name</p>";
+                    }
+
+                if (!empty($_REQUEST['lastname'])) { //This checks if LAST NAME is filled and stickies it. If not, it gives error message
+                    $lastname = $_REQUEST['lastname'];
+                    } else {
+                        $lastname = NULL;
+                        echo "<p class=\"error_message\">Please enter your last name</p>";
+                    }
+
+                if (!empty($_REQUEST['email'])) { //This checks if EMAIL is filled and stickies it. If not, it gives error message
+                    $email = $_REQUEST['email'];
+                    } else {
+                        $email = NULL;
+                        echo "<p class=\"error_message\">Please enter your email</p>";
+                        }
+
+                if (!empty($_REQUEST['password'])) { // This checks if PASSWORD is filled and stickies it. If not, it gives error message
+                    $password = $_REQUEST['password'];
+                    } else {
+                        $password = NULL;
+                        echo "<p class=\"error_message\">Please enter your password</p>";
+                        }
+
+                if (!empty($_REQUEST['password_confirm'])) { // This is checking if password confirm is filled. If it isn't filled, it gives an error message
+                    $password_confirm = $_REQUEST['password_confirm'];
+                    } else {
+                        $password_confirm = NULL;
+                        echo "<p class=\"error_message\">Please CONFIRM the password you created</p>";
+                        }
+
+                if ($password !== $password_confirm and !empty($password_confirm)) { // This checks if password/password confirmation match. If they don't, an error message is submitted. if they do match, a success message is submitted
+                        echo "<p class=\"error_message\">Password and password confirmation must match</p>";
+                        }
+                // END VALIDATION MESSAGES
+
+
+
+
 
     $insert_query = "INSERT INTO USER_FONGSURDENAS (first_name, last_name, email, password)
                     VALUES ($first_name, $last_name, $email, $password)"; //This is inserting into database. Parenthesis will be the column names being inserted into. This is SQL. It grabs the POST data and puts it into the query
